95f7802430fb84e65687c8a023d9d1473677548b
Mobcn <mobingcn@gmail.com>
2023-03-19 14:48:43 +0800

    init

--- src/api/chatgpt-service.js
+++ src/api/chatgpt-service.js
@@ -1,5 +1,5 @@
 /** 请求接口地址 */
-const url = '/api/send';
+const url = 'https://chat.mobingc.cn/api/send';
 
 /**
  * 发送消息
@@ -9,20 +9,53 @@ const url = '/api/send';
  * @returns {Promise<{role: 'assistant', content: string}>} 响应消息
  */
 export const sendMessages = async (messages, apiKey) => {
-    const response = await fetch(url, {
+    const result = { role: 'assistant', content: '' };
+    const msgs = [...messages];
+    // while (true) {
+    let response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ messages, apiKey })
+        body: JSON.stringify({ messages: msgs, apiKey })
     });
     if (!response.ok) {
         throw new Error('请求失败！');
     }
-    const res = await response.json();
-    if (res.code !== 0) {
+    let res = await response.json();
+    if (res.code !== 0 && res.code !== 1) {
         console.log(res.data.errorMessage);
         throw new Error('服务器错误！');
     }
-    return res.data.message;
+    result.content += res.data.message.content;
+    msgs.push(result);
+    return new Promise((rev) => {
+        setTimeout(async () => {
+            response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ messages: msgs, apiKey })
+            });
+            if (!response.ok) {
+                throw new Error('请求失败！');
+            }
+            res = await response.json();
+            if (res.code !== 0 && res.code !== 1) {
+                console.log(res.data.errorMessage);
+                throw new Error('服务器错误！');
+            }
+            result.content += res.data.message.content;
+            rev(result);
+        }, 1000);
+    });
+    // if (res.code === 0) {
+    //     break;
+    // }
+    // if (msgs.length === messages.length) {
+    //     msgs.push(result);
+    // }
+    // }
+    // return result;
 };
